7882c79b6f6c7e37f81ca2c9018d13970866b599
bgreenwell <greenwell.brandon@gmail.com>
2018-06-08 16:19:58 -0400

    fix logo

--- tools/logo-vip.R
+++ tools/logo-vip.R
@@ -1,12 +1,10 @@
 # Load required packages
-library(ggimage)
 library(ggplot2)
-library(grid)
-library(png)
 
 # Load vip image
-img <- rasterGrob(readPNG("tools/vip-img.png"),
-                  interpolate = TRUE, width = 0.85)
+img <- grid::rasterGrob(
+  png::readPNG("tools/logo-vip-img.png"), interpolate = TRUE, width = 0.85
+)
 
 # Hexagon data
 hex <- data.frame(x = 1.35 * 1 * c(-sqrt(3) / 2, 0, rep(sqrt(3) / 2, 2), 0,
@@ -23,7 +21,7 @@ g <- ggplot() +
                size = 3) +
   annotation_custom(img, xmin = -Inf, xmax = Inf, ymin = -Inf, ymax = Inf) +
   annotate(geom = "text", label = "vip", x = 0, y = -0.05,
-           family = "Open Sans Light", color = greys[7L], size = 7) +
+           family = "Open Sans Light", color = greys[9L], size = 7) +
   coord_equal(xlim = range(hex$x), ylim = range(hex$y)) +
   scale_x_continuous(expand = c(0.04, 0)) +
   scale_y_reverse(expand = c(0.04, 0)) +
@@ -41,12 +39,12 @@ g <- ggplot() +
         panel.grid.minor = element_blank())
 print(g)
 
-png("tools/vip-logo.png", width = 181, height = 209,
+png("tools/logo-vip.png", width = 181, height = 209,
     bg = "transparent", type = "cairo-png")
 print(g)
 dev.off()
 
-svg("tools/vip-logo.svg", width = 181 / 72, height = 209 / 72,
+svg("tools/logo-vip.svg", width = 181 / 72, height = 209 / 72,
     bg = "transparent")
 print(g)
 dev.off()
